07906e0f94a0b5928a6609cfc7d69d54f756b54e
Catalina <c.otarola.arriagada@gmail.com>
2020-10-12 19:49:12 -0700

    __init__.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from collections import Counter
+from pyramid import is_pyramid_word
 
 app = Flask(__name__)
 
@@ -9,31 +9,15 @@ def form():
 
 @app.route('/pyramid_word', methods=['GET', 'POST'])
 def pyramid_word():
-    # save input from the user into a variable
+    # take user input
     user_input = request.form['word']
-    # eliminate whitespaces
-    str = user_input.replace(" ", "")
-    # declare needed variables
-    result = ""
-    # check for empty string
-    if len(str) == 0:
-        result = "Not a pyramid word"
-        return render_template('result.html', result=result)
-    # initialize a counter
-    word_count = Counter()
-    # traverse the string to get the letters
-    for letter in str:
-        word_count[letter] += 1
-    # sorting count of letter
-    sorted_count = sorted(word_count.values())
-    # compare index and num, if they don't differ by one, the word is not a pyramid
-    for i, num in enumerate(sorted_count):
-        if num - i != 1:
-            result = "Not a pyramid word :("
-            return render_template('result.html', result=result)
-    result = "It's pyramid!"
-    return render_template('result.html', result=result)
-    
+    # check for pyramid
+    if is_pyramid_word(user_input):
+        msg = "It's pyramid!"
+    else:
+        msg = "Not a pyramid word :("
+    return render_template('result.html', msg=msg)
+
 
 if __name__=="__main_":
     app.run()
